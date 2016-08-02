45401472fcda2981f6fb1b9daf2ead0eb68e97eb
Christian Ternus <ternus@cternus.net>
2016-08-02 12:19:43 -0400

    initial commit

---
+++ mefizbot.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+
+import requests
+from textwrap import fill
+import zephyr
+from time import sleep
+from bs4 import BeautifulSoup
+URL = "http://www.metafilter.com/161257/Best-of-women-DNC-Day-4"
+CLASS = "mefi-auto"
+INSTANCE = "dnc"
+SENDER = "mefibot"
+
+res = None
+comment_count = 570
+
+zephyr.init()
+
+while True:
+    sleep(1)
+    try:
+        res = requests.get(URL)
+        parsed = BeautifulSoup(res.content, 'html.parser')
+        comments = parsed.findAll("div", "comments")[:-1]
+
+        print "Got %d/%d comments" % (comment_count, len(comments))
+
+        if comment_count == len(comments):
+            continue
+
+        new_comments = comments[comment_count:]
+        for comment in new_comments:
+            print "****"
+            ct = comment.text
+            commentmsg = fill(ct[:ct.rfind("posted by")])
+            commenter = ct[ct.rfind("posted by")+9:ct.rfind(' at')]
+            message = "%s\n --%s" % (commentmsg, commenter)
+            m = zephyr.ZNotice(message=message, cls=CLASS, sender=SENDER, \
+                               instance=INSTANCE)
+            m.send()
+        comment_count = len(comments)
+    except Exception as e:
+        print e, e.message
+
